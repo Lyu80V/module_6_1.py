383b0736cfb8bfe4aa87e03968627618d3cdbe25
Lyu80V <7222707@gmail.com>
2024-09-10 14:15:09 +0300

    Add files via upload

--- module_6_1.py
+++ module_6_1.py
@@ -7,11 +7,12 @@ class Animal:
 
     def eat(self, food):
         if food.edible == True:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
+            if self.alive == True:
+                print(f"{self.name} съел {food.name}")
+                self.fed = True
         else:
             print(f"{self.name} не стал есть {food.name}")
-            self.fed = False
+            self.alive = False
 
 
 class Plant:
