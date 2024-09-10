4774bf248e0d83df75d91384a2a48dc18e361058
Lyu80V <7222707@gmail.com>
2024-09-10 08:15:38 +0300

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,50 @@
+class Animal:
+    alive = True  # (живой)
+    fed = False  # (накормленный)
+
+    def __init__(self, name):
+        self.name = name  # индивидуальное название каждого животного.
+
+    def eat(self, food):
+        if food.edible == True:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.fed = False
+
+
+class Plant:
+    edible = False  # (съедобность),
+    def __init__(self, name):
+        self.name = name  # индивидуальное название каждого растения
+
+class Fruit(Plant):
+    edible = True
+class Flower(Plant):
+    pass
+
+class Mammal(Animal):
+    pass
+class Predator(Animal):
+    pass
+
+
+a1 = Predator('Серый волк')
+a2 = Mammal('Оленёнок Бемби')
+p1 = Flower('аленький цветочек')
+p2 = Fruit('мешок яблок')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a1.eat(p2)     # волк на диете
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
+
+
+
